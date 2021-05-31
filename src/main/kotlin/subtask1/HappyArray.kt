ab82d448dc205474b7ca545cbc0442d824e3e226
Павел Василевский <pavelvasilevsky@mail.ru>
2021-05-31 23:43:09 +0300

    Task 1

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,40 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
+        //find bad elements
+        for (i in 1 .. sadArray.size-2)
+        {
+            var n = 1
+            var x = i
+            while (sadArray[x] >= (sadArray[x-1]+sadArray[i+n])){
+                //println(sadArray[i])
+                sadArray[x] = -1
+                //x -= 1
+                //n += 1
+            }
+            x=i
+            n=1
+        }
+        //println("Sorted array: ${sadArray.contentToString()}")
+
+        //find size new array
+        var sizeNewArray = 0
+        sadArray.forEach {
+            if (it>=0)
+                sizeNewArray++
+        }
+
+        //create new array
+        val happyArray = IntArray(sizeNewArray) {0}
+        var k=0
+
+        sadArray.forEach {
+            if (it>=0){
+                happyArray[k] = it
+                k++}
+        }
+        return happyArray
+
         throw NotImplementedError("Not implemented")
     }
 }
